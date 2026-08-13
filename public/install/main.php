6df52cfeb1c3823c14515ba80753893a2fe1f902
ou <irving@pool-100-2-229-178.nycmny.fios.verizon.net>
2026-08-13 12:01:46 +0800

    fix: 安装向导添加管理员SQL指定列名，修复分站版site_id字段导致的登录失败

--- public/install/main.php
+++ public/install/main.php
@@ -39,8 +39,16 @@ $salt = genRandomString(4);
 $time = time();
 $ip = get_client_ip();
 $password = sha1($password . $salt . $password . $salt);
-$url = "insert into `{$dbPrefix}admin` VALUES (1,'{$username}', '{$password}', '{$salt}', '超级管理员','','超级管理员','',0.00,1,'127.0.0.1',0,'{$time}','{$time}')";
-$mysqli->query($url);
+// 分站版 qf_admin 表在 admin_group 之后新增了 site_id 字段，
+// 必须显式指定列名，否则插入会错位（严格模式下直接失败，管理员写不进去）
+$username = $mysqli->real_escape_string($username);
+$url = "insert into `{$dbPrefix}admin` (`admin_id`,`admin_account`,`admin_password`,`admin_salt`,`admin_name`,`admin_idcard`,`admin_truename`,`admin_email`,`admin_money`,`admin_group`,`admin_ipreg`,`admin_status`,`admin_createtime`,`admin_updatetime`) VALUES (1,'{$username}', '{$password}', '{$salt}', '超级管理员','','超级管理员','',0.00,1,'127.0.0.1',0,'{$time}','{$time}')";
+$result = $mysqli->query($url);
+if (!$result) {
+    $error = $mysqli->error;
+    $mysqli->close();
+    return array('status'=>1,'info'=>'添加管理员失败：'.$error);
+}
 
 $mysqli->close();
 return array('status'=>2,'info'=>'成功添加管理员<br />成功写入配置文件<br>安装完成...');
